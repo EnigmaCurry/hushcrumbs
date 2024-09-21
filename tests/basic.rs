2aa823e3f380e4e87ff7880e6dcc3810074b58b3
EnigmaCurry <ryan@enigmacurry.com>
2024-09-21 13:23:11 -0600

    test cache2

--- tests/basic.rs
+++ tests/basic.rs
@@ -18,7 +18,7 @@ fn test_init() {
 fn test_file_add() {
     let context = TestBed::new();
     context.run("init test t").assert().success();
-    context.shell("touch salut.txt").assert().success();
+    context.shell("touch hi.txt").assert().success();
     context.shell("ls").assert().success();
-    context.run("add test salut.txt").assert().success();
+    context.run("add test hi.txt").assert().success();
 }
