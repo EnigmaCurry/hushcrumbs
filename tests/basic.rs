7a1b56c91234ec7f3b980e99f47484ca3563d82b
EnigmaCurry <ryan@enigmacurry.com>
2024-09-21 22:03:39 -0600

    test restore

--- tests/basic.rs
+++ tests/basic.rs
@@ -83,3 +83,40 @@ fn test_file_remove() {
     assert_path_is_symlink(hello); // This file is still backed up
     assert_regular_file_exists(howdy); // This file is restored, no longer backed up.
 }
+
+#[test]
+fn test_restore() {
+    let context = TestBed::new();
+
+    context.run("init test t").assert().success();
+    context.shell("touch hi.txt").assert().success();
+    context.shell("touch hello.txt").assert().success();
+    context.shell("touch bonjour.txt").assert().success();
+    context.shell("touch howdy.txt").assert().success();
+    context.run("add test hi.txt").assert().success();
+    context.run("add test hello.txt").assert().success();
+    context.run("add test bonjour.txt").assert().success();
+    context.run("add test howdy.txt").assert().success();
+    context
+        .shell("rm -f hi.txt hello.txt bonjour.txt howdy.txt")
+        .assert()
+        .success();
+
+    let hi = &format!("{}/hi.txt", context.temp_dir_path);
+    let hello = &format!("{}/hello.txt", context.temp_dir_path);
+    let bonjour = &format!("{}/bonjour.txt", context.temp_dir_path);
+    let howdy = &format!("{}/howdy.txt", context.temp_dir_path);
+
+    // These files have been deleted, but are still backed up:
+    assert_path_not_exists(hi);
+    assert_path_not_exists(hello);
+    assert_path_not_exists(bonjour);
+    assert_path_not_exists(howdy);
+
+    context.run("restore test").assert().success();
+
+    assert_path_is_symlink(hi);
+    assert_path_is_symlink(hello);
+    assert_path_is_symlink(bonjour);
+    assert_path_is_symlink(howdy);
+}
